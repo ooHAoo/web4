0d79e1905feb7c5cfe8f9ff03c569aca1603483a
Huda <hal-dallal@myseneca.ca>
2018-01-05 22:35:37 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB422 –
+Assignment07
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
+* 
+*  Name: Huda Al Dallal Student ID: 130268162 Date: Jan 5, 20017
+*
+*  Heroku Link: _______________________________________________________________
+*
+********************************************************************************/ 
 import { Component } from '@angular/core';
 
 @Component({
